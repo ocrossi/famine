77a99c37f98851260fb608edb646ca7c24fcab9c
ocrossi <octave.rossi@gmail.com>
2025-12-19 09:40:19 +0100

    setup.sh to test /tmp/test2

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 dir='/tmp/test'
+dir2='/tmp/test2'
 
-rm -rf $dir
+rm -rf $dir $dir2
 
 mkdir -p $dir/depth1/depth2
-
 echo toto > $dir/file1
 echo toto > $dir/depth1/file2
+mkdir -p $dir2/depth1/
 
 cp /bin/ls $dir 
 cp /bin/ls $dir/depth1/depth2/
+cp /bin/ls $dir2/depth1/ 
+
 
 make re 
 
